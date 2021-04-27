7674a14762a5e3e00139caf96d2ff62eef4ebd43
Mason del Rosario <mason.delro@gmail.com>
2021-04-26 17:23:32 -0700

    Add matrix format for cosine similarity

--- utils/cosine_sim_performance.py
+++ utils/cosine_sim_performance.py
@@ -1,4 +1,23 @@
 import numpy as np
+from tqdm import tqdm
+
+def cosine_similarity_mat(A, B, pow_diff_T=None):
+    """ 
+    -> np.array form; use numpy builtins
+    -> return average cosine similarity between elements in estimate and ground truth  (A and B)
+    A.shape = B.shape = (n_samples, T, n_freq, n_ang)
+    """
+    N, T, n_freq, n_ant = A.shape
+    rho_trunc = 0
+    rho = 0
+    for i in tqdm(range(N)):
+        A_i = A[i,:,:,:]
+        B_i = B[i,:,:,:]
+        num = np.abs(np.sum(np.multiply(A_i,np.conj(B_i)), axis=2))
+        A_norm = np.sqrt(np.sum(np.multiply(A_i,np.conj(A_i)), axis=2))
+        B_norm = np.sqrt(np.sum(np.multiply(B_i,np.conj(B_i)), axis=2))
+        rho += np.sum(num / (A_norm * B_norm)) / (N*T*n_freq)
+    return np.real(rho)
 
 def cosine_similarity(A, B, pow_diff_T=None):
     """ 
@@ -8,6 +27,7 @@ def cosine_similarity(A, B, pow_diff_T=None):
     N, T, n_freq, n_ant = A.shape
     rho_trunc = 0
     # rho_all = 0
+    n_samples = 0
     for i in range(N):
         for t in range(T):
             # assume power uniformly distributed among angles
@@ -18,14 +38,16 @@ def cosine_similarity(A, B, pow_diff_T=None):
                 AB_dot = np.abs(np.dot(A_i, np.conj(B_i)))
                 A_norm = np.sqrt(np.sum(A_i*np.conj(A_i)))
                 B_norm = np.sqrt(np.sum(B_i*np.conj(B_i))) 
-                rho_trunc += AB_dot / (A_norm*B_norm) / (T*n_ant*N)
-                # rho_all += AB_dot / (A_norm*(B_norm+pow_diff)) / (T*n_ant*N)
+                # if A_norm > 0 and B_norm > 0:
+                #     rho_trunc += AB_dot / (A_norm*B_norm) 
+                #     n_samples += 1
+                rho_trunc += AB_dot / (A_norm*B_norm) / (T*n_freq*N)
     # return [np.real(rho_trunc), np.real(rho_all)]
     return np.real(rho_trunc)
 
 if __name__ == "__main__":
     N = 100
-    n_del = 32
+    n_del = 64
     n_ang = 32
     T = 10
     A = np.random.normal(size=(N,T,n_del,n_ang)) + 1j*np.random.normal(size=(N,T,n_del,n_ang))
@@ -33,10 +55,12 @@ if __name__ == "__main__":
     sigma_list = [0.1,1.0,10.0]
     print(f"--- Testing: cosine_similarity(A,B) for B = A + CN(0,sigma) ---")
     A_rho = cosine_similarity(A,A)
-    print(f"sigma={0:2.1E} -> cos(A,A): {A_rho}")
+    A_rho_mat = cosine_similarity_mat(A,A)
+    print(f"sigma={0:2.1E} -> cos(A,A): {A_rho} - cos_mat(A,A): {A_rho_mat}")
     for sigma in sigma_list:
         B = A + np.random.normal(0,sigma,size=(N,T,n_del,n_ang)) + 1j*np.random.normal(0,sigma,size=(N,T,n_del,n_ang))
         # AB_rho, AB_rho_all = cosine_similarity(A,B,pow_diff_T=pow_diff)
         # print(f"sigma={sigma:2.1E} -> cos_truncate(A,B): {AB_rho} - cos_all(A,B): {AB_rho_all}")
         AB_rho = cosine_similarity(A,B)
-        print(f"sigma={sigma:2.1E} -> cos_truncate(A,B): {AB_rho}")
+        AB_rho_mat = cosine_similarity_mat(A,B)
+        print(f"sigma={sigma:2.1E} -> cos_truncate(A,B): {AB_rho} - cos_truncate_mat(A,B): {AB_rho_mat}")
