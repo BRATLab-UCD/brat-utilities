db65c3d967f631221902f98ce668c53450d3b79e
Mason del Rosario <mason.delro@gmail.com>
2021-05-27 08:34:08 -0700

    Add magH3 renorm/denorm. Add thresh_idx_path o norm_sphH4.

--- utils/NMSE_performance.py
+++ utils/NMSE_performance.py
@@ -108,7 +108,7 @@ def denorm_tanh(data, meanvar_file, n_stddev=4, eps=0.01):
     return data
 
 ### helper function: denormalize H4 with spherical normalization
-def denorm_sphH4(data, minmax_file, t1_power_file, batch_num, link_type='down', timeslot=0):
+def denorm_sphH4(data, minmax_file, t1_power_file, batch_num, link_type='down', timeslot=0, thresh_idx_path=False):
     # iterate through batches, denormalize based on extrema of given timeslot
     with open(f"{minmax_file}", "rb") as f:
         extrema_dict = pickle.load(f)
@@ -126,17 +126,25 @@ def denorm_sphH4(data, minmax_file, t1_power_file, batch_num, link_type='down', 
         batch_power = pickle.load(f)
         f.close()
     link_power = batch_power[f"pow_{link_type}"]
+    if thresh_idx_path != False:
+        thresh_idx = np.squeeze(sio.loadmat(f"{thresh_idx_path}")["i_percent"]) - 1 # subtract one from matlab idx
+        print(f"thresh_idx.shape: {thresh_idx.shape}\nthresh_idx: {thresh_idx}")
+        link_power = link_power[thresh_idx]
     temp = np.reshape(data, (data.shape[0], -1)) * link_power[:, None]
     data = np.reshape(temp, data.shape)
     return data
 
 ### helper function: renormalize H4 with spherical normalization
-def renorm_sphH4(data, minmax_file, t1_power_file, batch_num, link_type='down', timeslot=0):
+def renorm_sphH4(data, minmax_file, t1_power_file, link_type='down', timeslot=0, thresh_idx_path=False):
     # with open(f"{t1_power_file}1.pkl", "rb") as f:
     with open(f"{t1_power_file}.pkl", "rb") as f:
         batch_power = pickle.load(f)
         f.close()
     link_power = batch_power[f"pow_{link_type}"]
+    if thresh_idx_path != False:
+        thresh_idx = np.squeeze(sio.loadmat(f"{thresh_idx_path}")["i_percent"]) - 1 # subtract one from matlab idx
+        print(f"thresh_idx.shape: {thresh_idx.shape}\nthresh_idx: {thresh_idx}")
+        link_power = link_power[thresh_idx]
     data = np.reshape(np.reshape(data, (data.shape[0], -1)) / link_power[:,None], data.shape)
     # perform minmax scaling on estimates
     with open(f"{minmax_file}", "rb") as f:
@@ -219,6 +227,85 @@ def renorm_sph_magH3(data, minmax_file, t1_power_file, batch_num, link_type='dow
         d_max = np.max(extrema[1])
     data_mag = (data_mag-d_min)/(d_max-d_min)
     data = np.concatenate([np.expand_dims(data_mag, concat_axis), np.expand_dims(data_ang, concat_axis)], axis=concat_axis)
+    return data
+
+### helper function: renormalize H3 [0,1] with minmax normalization, magnitude/phase
+def renorm_magH3(data, minmax_file, link_type='down', timeslot=0):
+    # split mag/phase
+    if len(data.shape) == 4:
+        data = data[:,0,:,:]+data[:,1,:,:]*1j
+        concat_axis = 1
+    elif len(data.shape) == 5:
+        data = data[:,:,0,:,:]+data[:,:,1,:,:]*1j
+        concat_axis = 2
+    else:
+        print("--- renorm_magH3: data are not correct shape. Expected 4 or 5 axes. ---")
+        return None
+    data_mag = np.abs(data)
+    data_ang = np.angle(data)
+
+    # load extrema
+    fieldnames = ['link','min','max']
+    if "csv" in minmax_file:
+        with open(minmax_file) as csv_file:
+            csv_reader = csv.DictReader(csv_file,fieldnames=fieldnames,delimiter=',')
+            for row in csv_reader:
+                if row['link']==link_type:
+                    d_min = float(row['min'])
+                    d_max = float(row['max'])
+    elif "pkl" in minmax_file:
+        with open(f"{minmax_file}", "rb") as f:
+            extrema_dict = pickle.load(f)
+            f.close()
+        extrema = extrema_dict[f"H_{link_type}_ext"]
+        if timeslot != -1:
+            d_min, d_max = np.min(extrema[0]), np.max(extrema[1]) # assume single timeslot performance
+        else:
+            d_min, d_max = extrema[0][timeslot], extrema[1][timeslot] # assume single timeslot performance
+
+    # perform minmax scaling on magnitude estimates
+    print(f"--- renorm_magH3: d_min={d_min}, d_max={d_max}---")
+    data_mag = (data_mag-d_min)/(d_max-d_min)
+    data = np.concatenate([np.expand_dims(data_mag, concat_axis), np.expand_dims(data_ang, concat_axis)], axis=concat_axis)
+    return data
+
+def denorm_magH3(data, minmax_file, link_type='down', timeslot=0):
+    # undo minmax scaling on magnitude estimates
+
+    fieldnames = ['link','min','max']
+    if "csv" in minmax_file:
+        with open(minmax_file) as csv_file:
+            csv_reader = csv.DictReader(csv_file,fieldnames=fieldnames,delimiter=',')
+            for row in csv_reader:
+                if row['link']==link_type:
+                    d_min = float(row['min'])
+                    d_max = float(row['max'])
+    elif "pkl" in minmax_file:
+        with open(f"{minmax_file}", "rb") as f:
+            extrema_dict = pickle.load(f)
+            f.close()
+        extrema = extrema_dict[f"H_{link_type}_ext"]
+        if timeslot != -1:
+            d_min, d_max = np.min(extrema[0]), np.max(extrema[1]) # assume single timeslot performance
+        else:
+            d_min, d_max = extrema[0][timeslot], extrema[1][timeslot] # assume single timeslot performance
+
+    if len(data.shape) == 4:
+        data_mag, data_pha = data[:,0,:,:], data[:,1,:,:]
+        concat_axis = 1
+    elif len(data.shape) == 5:
+        data_mag, data_pha = data[:,:,0,:,:], data[:,:,1,:,:]
+        concat_axis = 2
+    else:
+        print("--- renorm_sph_magH3: data are not correct shape. Expected 4 or 5 axes. ---")
+        return None
+    # data_mag = np.clip(data_mag, 0, 1) # trying this; avoid negative magnitude values 
+    data_mag = data_mag*(d_max-d_min) + d_min
+    # incoming data channels are [mag, phase]; convert to re/im
+    data_re = np.expand_dims(data_mag*np.cos(data_pha), axis=concat_axis)
+    data_im = np.expand_dims(data_mag*np.sin(data_pha), axis=concat_axis)
+    data = np.concatenate((data_re, data_im), axis=concat_axis)
+
     return data
 
 # calculate NMSE
